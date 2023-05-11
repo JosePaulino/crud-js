98a920ab453cbd391128fd363ebfd651c9d18e01
José Paulino <j.paulino1@hotmail.com>
2023-05-10 23:02:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+// Função para adcionar nova linha na tabela
+function addTabela() {
+  //Define as variaveis e define os dados
+  let nome = document.getElementById('nome').value
+  let email = document.getElementById('email').value
+  let fone = document.getElementById('fone').value
+  let curso = document.getElementById('curso').value
+  let table = document.getElementById('Mytable')
+
+  let tableSize = table.row.length // Calculando o tamanho da tabela
+  let row = table.insertRow(tableSize) //Inserindo uma linha abaixo da Tabela
+  let cell1 = row.insertCell(0) // Inserindo as celulas da linha
+  let cell2 = row.insertCell(1)
+  let cell3 = row.insertCell(2)
+  let cell4 = row.insertCell(3)
+  let cell5 = row.insertCell(4)
+  let cell6 = row.insertCell(5)
+  row.id = tableSize //Adicionando o id no elemento a ser criado
+
+  // Criando o codigo do botao para remover a linha
+  let btnCode =
+    "<button class='remove-btn' onclick='removeTable(this)'>Remover</button>"
+
+  //Preenchendo as celulas da linha
+  cell1.innerHtml = tableSize
+  cell2.innerHtml = nome
+  cell3.innerHtml = email
+  cell4.innerHtml = fone
+  cell5.innerHtml = curso
+  cell6.innerHtml = btnCode
+
+  //Limpando os campos de insercao de dados
+  document.getElementById('nome').value = ''
+  document.getElementById('email').value = ''
+  document.getElementById('fone').value = ''
+  document.getElementById('curso').value = ''
+
+  //Retornando 'false' para impedir o reload da pagina
+  return false
+}
+
+//Função para remover uma linha
+function removeTable(id) {
+  let row = id.parentNode.parentNode.id //Pegando o id do avô do botao
+  row = document.getElementById(row) // Recebendo o elemento da linha pelo ID
+  row.parentNode.removeChild(row) //Removendo a linha
+
+  //Retornando 'false' para impedir o reload da pagina
+  return false
+}
